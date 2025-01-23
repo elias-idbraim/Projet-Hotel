229d6e77c63569793203872794405e77009e2a31
Elias IDBRAIM <IDBRAIM_El@lprs.local>
2025-01-23 15:03:45 +0100

    Initialisation du fichier

---
+++ src/application/Application.java
@@ -0,0 +1,46 @@
+package application;
+import 
+public class Application {
+    public static void main(String[] args) {
+        // Création de clients
+        Client client1 = new Client("Alice Dupont", "alice@mail.com");
+        Client client2 = new Client("Bob Martin", "bob@mail.com");
+        // Création de chambres
+        Chambre chambre1 = new Chambre(101, "simple");
+        Chambre chambre2 = new Chambre(102, "double");
+        Chambre chambre3 = new Chambre(103, "suite");
+        // Création d'un hôtel
+        Hotel hotel = new Hotel("L'hotel du Père Noël","Avenue des Rennes","Laponie");
+        // Ajouter des chambres à l'hôtel
+        hotel.ajouterChambre(chambre1);
+        hotel.ajouterChambre(chambre2);
+        hotel.ajouterChambre(chambre3);
+        // Affichage des chambres disponibles
+        hotel.afficherChambresDisponibles();
+        // Réservation d'une chambre
+        String dateDebut = "2024-12-21";
+        String dateFin = "2024-12-28";
+        hotel.reserverChambre(client1, chambre1, dateDebut, dateFin);
+        // Affichage des réservations
+        hotel.afficherReservations();
+        // Réservation d'une chambre déjà réservée
+        hotel.reserverChambre(client2, chambre1, dateDebut, dateFin);
+
+        // Affichage des chambres disponibles
+        hotel.afficherChambresDisponibles();
+
+        // Changement de la réservation du client1 et de la chambre 1 à la chambre2
+        hotel.changerAffectation(client1,chambre1,chambre2);
+
+        // Réservation d'une chambre
+        hotel.reserverChambre(client2, chambre1, dateDebut, dateFin);
+
+        hotel.afficherReservations();
+        hotel.afficherChambresDisponibles();
+
+        // Supression d'une réservation
+        hotel.supressionReservation(client1,chambre2);
+
+        hotel.afficherReservations();
+        hotel.afficherChambresDisponibles();
+    }}
